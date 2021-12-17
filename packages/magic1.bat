4c4b545b6854a022ee7663d5d2cc136cd427cd0f
Protonos <96314060+Protonosgit@users.noreply.github.com>
2021-12-17 21:47:17 +0100

    Update magic1.bat

--- packages/magic1.bat
+++ packages/magic1.bat
@@ -1,3 +1,22 @@
 @echo off
-ECHO MAGIC1 start
+ECHO ---MAGIC1---
+ECHO QuiMA Setup-script
 PAUSE
+
+cls
+ECHO 1: base
+ECHO 2 :quick
+ECHO 3: break
+ECHO ----------
+SET /P M=Select 1-3 then press ENTER:
+IF %M%==1 GOTO A
+IF %M%==2 GOTO B
+IF %M%==3 GOTO C
+
+:A
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/Protonosgit/quima-setup/main/downhill.png c:tmp\quima\downhill.png
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d c:\tmp\quima\downhill.png /f
+
+:B
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+
