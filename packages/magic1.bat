752658c509c75c55cc958cf17e41be170fdadf36
Protonos <96314060+Protonosgit@users.noreply.github.com>
2021-12-23 15:29:11 +0100

    Update magic1.bat

--- packages/magic1.bat
+++ packages/magic1.bat
@@ -14,11 +14,20 @@ IF %M%==2 GOTO B
 IF %M%==3 GOTO C
 
 :A
+ECHO Base setup
+PAUSE
 bitsadmin /transfer quimadown /download /priority normal https://raw.githubusercontent.com/Protonosgit/quima-setup/main/downhill.png c:\tmp\quima\wall.png
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d c:\tmp\quima\wall.png /f
-set imgPath=c:\tmp\quima\wall.png
-[Win32.Wallpaper]::SetWallpaper($imgPath)
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\tmp\quima\wall.png /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
 
 :B
-$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+ECHO Blender setup
+PAUSE
+https://www.blender.org/download/release/Blender3.0/blender-3.0.0-windows-x64.msi/
+
+:C
+ECHO Cloud gaming setup
+PAUSE
+ECHO not finished
 
